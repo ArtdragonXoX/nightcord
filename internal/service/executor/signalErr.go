65976695bb6134f42c551675ca514756c4b471e7
ArtDragonXoX <1262922587@qq.com>
2025-03-09 14:49:48 +0800

    feat(internal/service/executor): 添加信号错误处理函数
    - 新增 SignalStatus 函数，根据信号类型返回对应的错误状态码
    - 新增 SignalMessage 函数，根据信号类型返回对应的错误信息
    - 支持处理 SIGSEGV、SIGXFSZ、SIGFPE、SIGABRT 等常见信号
    - 为 Linux 平台特有功能，使用 build tag 进行条件编译

---
+++ internal/service/executor/signalErr.go
@@ -0,0 +1,41 @@
+//go:build linux
+// +build linux
+
+package executor
+
+import (
+	"nightcord-server/internal/model"
+	"syscall"
+)
+
+func SignalStatus(s syscall.Signal) model.StatusId {
+	switch s {
+	case syscall.SIGSEGV:
+		return model.StatusRESIGSEGV
+	case syscall.SIGXFSZ:
+		return model.StatusRESIGXFSZ
+	case syscall.SIGFPE:
+		return model.StatusRESIGFPE
+	case syscall.SIGABRT:
+		return model.StatusRESIGABRT
+	default:
+		return model.StatusRE
+	}
+}
+
+func SignalMessage(s syscall.Signal) string {
+	switch s {
+	case syscall.SIGSEGV:
+		return "内存段错误"
+	case syscall.SIGXFSZ:
+		return "文件大小限制超出"
+	case syscall.SIGFPE:
+		return "算术运算错误"
+	case syscall.SIGABRT:
+		return "程序异常终止"
+	case syscall.SIGSYS:
+		return "系统调用错误"
+	default:
+		return "未知错误"
+	}
+}
