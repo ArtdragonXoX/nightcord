d100b0893d7424d4ac968b2b2a4855f65a8baf74
ArtDragonXoX <1262922587@qq.com>
2025-03-10 10:40:43 +0800

    feat(config): 调整内存限制默认值
    - 将 compile_memory 和 memory_limit 的默认值从 256 调整为 262144
    - 在 ExecutorConf 结构体中添加了对内存限制单位的注释

--- internal/conf/executor.go
+++ internal/conf/executor.go
@@ -3,10 +3,10 @@ package conf
 type ExecutorConf struct {
 	Pool           int     `yaml:"pool" json:"pool"`
 	ExtraCPUTime   float64 `yaml:"extra_cpu_time" json:"extra_cpu_time"`
-	CompileTimeout float64 `yaml:"compile_timeout"`
-	CompileMemory  int     `yaml:"compile_memory"`
-	CPUTimeLimit   float64 `yaml:"cpu_time_limit"`
-	MemoryLimit    uint    `yaml:"memory_limit"`
+	CompileTimeout float64 `yaml:"compile_timeout"` // seconds
+	CompileMemory  int     `yaml:"compile_memory"`  // KB
+	CPUTimeLimit   float64 `yaml:"cpu_time_limit"`  // seconds
+	MemoryLimit    uint    `yaml:"memory_limit"`    // KB
 }
 
 func (c *ExecutorConf) Default() {
@@ -20,12 +20,12 @@ func (c *ExecutorConf) Default() {
 		c.CompileTimeout = 5
 	}
 	if c.CompileMemory == 0 {
-		c.CompileMemory = 256
+		c.CompileMemory = 262144
 	}
 	if c.CPUTimeLimit == 0 {
 		c.CPUTimeLimit = 5
 	}
 	if c.MemoryLimit == 0 {
-		c.MemoryLimit = 256
+		c.MemoryLimit = 262144
 	}
 }
