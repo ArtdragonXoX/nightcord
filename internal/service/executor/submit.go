87eba229cb095e31efce531a5341cfa93c95d299
ArtDragonXoX <1262922587@qq.com>
2025-05-11 22:45:27 +0800

    feat(executor): 添加对单测试用例类型的支持
    在SubmitJob函数中添加了对单测试用例类型的处理，当TestcaseType为SingleTest时，将Stdin和ExpectedOutput转换为Testcase数组，以确保与现有逻辑兼容

--- internal/service/executor/submit.go
+++ internal/service/executor/submit.go
@@ -6,5 +6,13 @@ import (
 
 // SubmitJob 提交评测任务到消息队列，并阻塞等待执行结果返回
 func SubmitJob(req model.SubmitRequest) model.JudgeResult {
+	if req.TestcaseType == model.SingleTest {
+		req.Testcase = []model.Testcase{
+			{
+				Stdin:          req.Stdin,
+				ExpectedOutput: req.ExpectedOutput,
+			},
+		}
+	}
 	return GetJobManagerInstance().SubmitJob(req)
 }
