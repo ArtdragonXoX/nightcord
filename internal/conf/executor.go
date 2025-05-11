214739a61fcb714121631e72301d57dbfb717ddd
ArtDragonXoX <1262922587@qq.com>
2025-05-11 22:37:00 +0800

    feat(executor): 添加任务队列和运行任务队列配置
    为ExecutorConf结构体添加JobQueue和RunQueue字段，并设置默认值。这些配置项用于控制任务队列和运行任务队列的大小，以提高系统的并发处理能力。

--- internal/conf/executor.go
+++ internal/conf/executor.go
@@ -1,7 +1,9 @@
 package conf
 
 type ExecutorConf struct {
+	JobQueue       int     `yaml:"job_queue" json:"job_queue"`           // 任务队列大小
 	JobPool        int     `yaml:"job_pool" json:"job_pool"`             // 任务协程池池数量
+	RunQueue       int     `yaml:"run_queue" json:"run_queue"`           // 运行任务队列大小
 	RunPool        int     `yaml:"run_pool" json:"run_pool"`             // 运行协程池数量
 	ExtraCPUTime   float64 `yaml:"extra_cpu_time" json:"extra_cpu_time"` // seconds 在超出限制时间后的额外时间
 	CompileTimeout float64 `yaml:"compile_timeout"`                      // seconds 最大编译时间
@@ -11,8 +13,14 @@ type ExecutorConf struct {
 }
 
 func (c *ExecutorConf) Default() {
+	if c.JobQueue == 0 {
+		c.JobQueue = 500
+	}
 	if c.JobPool == 0 {
 		c.JobPool = 5
+	}
+	if c.RunQueue == 0 {
+		c.RunQueue = 500
 	}
 	if c.RunPool == 0 {
 		c.RunPool = 5
